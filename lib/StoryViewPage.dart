faae63df94824bf4c967dea44bd296bccfbba8eb
Fabian Camilo Pineda Rendon <fabian.pineda@ludycom.com>
2025-08-19 07:29:14 -0500

    se ajustan temas de los tiempos

--- lib/StoryViewPage.dart
+++ lib/StoryViewPage.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class StoryViewPage extends StatefulWidget {
@@ -13,6 +14,9 @@ class _StoryViewPageState extends State<StoryViewPage> {
   final PageController _pageController = PageController(
 
   );
+  int _currentPage = 0;
+  late List<double> _progressValues;
+  Timer? _timer;
  /* final List<String> storyAssets = [
     'assets/img_1.png',
     'assets/img_2.png',
@@ -20,39 +24,81 @@ class _StoryViewPageState extends State<StoryViewPage> {
     'assets/img_4.png',
     'assets/img_5.png',
   ];*/
-  final List<String> storyAssets = [
+  /*final List<String> storyAssets = [
     'https://images.unsplash.com/photo-1506744038136-46273834b3fb',
     'https://images.unsplash.com/photo-1495567720989-cebdbdd97913',
     'https://images.unsplash.com/photo-1522206024047-9c925421675b',
-    'assets/img_4.png',
     'https://images.unsplash.com/photo-1470770903676-69b98201ea1c',
+  ];*/
+
+  final List<StoryPageInfo> storyAssets = [
+    StoryPageInfo(url: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb', isVideo: false, duration:Duration(seconds: 5)),
+    StoryPageInfo(url: 'https://images.unsplash.com/photo-1495567720989-cebdbdd97913', isVideo: false, duration: Duration(seconds: 5)),
+    StoryPageInfo(url: 'https://images.unsplash.com/photo-1522206024047-9c925421675b', isVideo: false, duration: Duration(seconds: 5)),
+    StoryPageInfo(url: 'https://images.unsplash.com/photo-1470770903676-69b98201ea1c', isVideo: false, duration: Duration(seconds: 5)),
+  ];
+  final List<NetworkImage> images=[
+    NetworkImage('https://images.unsplash.com/photo-1506744038136-46273834b3fb'),
+    NetworkImage('https://images.unsplash.com/photo-1495567720989-cebdbdd97913'),
+    NetworkImage('https://images.unsplash.com/photo-1522206024047-9c925421675b'),
+    NetworkImage('https://images.unsplash.com/photo-1470770903676-69b98201ea1c'),
   ];
 
-  int _currentPage = 0;
-  late List<double> _progressValues;
-  Timer? _timer;
+  List<StoryContent> storyWidgets = [];
+
+
+
+
 
   static const Duration storyDuration = Duration(seconds: 5);
   static const Duration tick = Duration(milliseconds: 10);
 
   @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    // Pre-cargar imágenes una sola vez
+
+  }
+
+
+  @override
   void initState() {
     super.initState();
     _progressValues = List.generate(storyAssets.length, (_) => 0.0);
+    storyWidgets= List.generate(storyAssets.length, (index) {
+      return StoryContent(
+        imagePath: storyAssets[index].url,
+        onBack: _goToBackStory,
+        onNext: _onTapNextHistory,
+        onPause: () => _timer?.cancel(),
+        onResume: _startProgress,
+      );
+
+    });
+
     //_startProgress();
+
   }
 
   void _startProgress() {
+    if(_timer?.isActive==true ){
+        return;
+    }
+    if( _progressValues[_currentPage]==1.0 && _currentPage == storyAssets.length - 1){
+      _timer?.cancel();
+      return;
+    }
     _timer?.cancel();
     _timer = Timer.periodic(tick, (timer) {
       setState(() {
         _progressValues[_currentPage] +=
-            tick.inMilliseconds / storyDuration.inMilliseconds;
+            tick.inMilliseconds / storyAssets[_currentPage].duration.inMilliseconds;
 
         if (_progressValues[_currentPage] >= 1.0) {
           _progressValues[_currentPage] = 1.0;
           timer.cancel();
-          _goToNextStory();
+          _onTapNextHistory();
         }
       });
     });
@@ -69,11 +115,12 @@ class _StoryViewPageState extends State<StoryViewPage> {
         duration: Duration(milliseconds: 10),
         curve: Curves.easeInOut,
       );
-      //_startProgress();
+
     } else {
-      _timer?.cancel();
+     // _timer?.cancel();
      // Navigator.pop(context); // cerrar al final
     }
+    _startProgress();
   }
 
   void _goToBackStory() {
@@ -88,8 +135,8 @@ class _StoryViewPageState extends State<StoryViewPage> {
         duration: Duration(milliseconds: 10),
         curve: Curves.easeInOut,
       );
-      //_startProgress();
     }
+    _startProgress();
   }
 
   void _onTapNextHistory() {
@@ -115,13 +162,7 @@ class _StoryViewPageState extends State<StoryViewPage> {
             physics: NeverScrollableScrollPhysics(),
             itemCount: storyAssets.length,
             itemBuilder: (context, index) {
-              return StoryContent(
-                imagePath: storyAssets[index],
-                onBack: _goToBackStory,
-                onNext: _onTapNextHistory,
-                onPause: () => _timer?.cancel(),
-                onResume: _startProgress,
-              );
+              return storyWidgets[index];
             },
           ),
           Positioned(
@@ -193,7 +234,8 @@ class StoryTopBar extends StatelessWidget {
   }
 }
 
-class StoryContent extends StatelessWidget {
+
+class StoryContent extends StatefulWidget {
   final String imagePath;
   final VoidCallback onBack;
   final VoidCallback onNext;
@@ -209,29 +251,36 @@ class StoryContent extends StatelessWidget {
     required this.onResume,
   });
 
+  @override
+  State<StoryContent> createState() => _StoryContentState();
+}
 
+class _StoryContentState extends State<StoryContent>
+    with AutomaticKeepAliveClientMixin {
   bool _isNetworkImage(String path) {
     return path.startsWith('http://') || path.startsWith('https://');
   }
 
   @override
   Widget build(BuildContext context) {
+    super.build(context); // 👈 importante para que AutomaticKeepAliveClientMixin funcione
+
     return GestureDetector(
-      onLongPress: onPause,
-      onLongPressEnd: (_) => onResume(),
+      onLongPress: widget.onPause,
+      onLongPressEnd: (_) => widget.onResume(),
       child: Stack(
         fit: StackFit.expand,
         children: [
-          _isNetworkImage(imagePath)
+          _isNetworkImage(widget.imagePath)
               ? Image.network(
-            imagePath,
+            widget.imagePath,
             fit: BoxFit.cover,
             loadingBuilder: (context, child, loadingProgress) {
-              if (loadingProgress == null){
-                onResume();
+              if (loadingProgress == null) {
+                widget.onResume();
                 return child;
-              } else{
-                onPause();
+              } else {
+                widget.onPause();
               }
               return Center(
                 child: CircularProgressIndicator(
@@ -244,20 +293,20 @@ class StoryContent extends StatelessWidget {
               );
             },
             errorBuilder: (context, error, stackTrace) {
-              return Center(
+              return const Center(
                 child: Icon(Icons.broken_image, color: Colors.white),
               );
             },
           )
               : Image.asset(
-            imagePath,
+            widget.imagePath,
             fit: BoxFit.cover,
           ),
           SizedBox.expand(
             child: Row(
               children: <Widget>[
-                Expanded(child: GestureDetector(onTap: onBack)),
-                Expanded(child: GestureDetector(onTap: onNext)),
+                Expanded(child: GestureDetector(onTap: widget.onBack)),
+                Expanded(child: GestureDetector(onTap: widget.onNext)),
               ],
             ),
           ),
@@ -265,7 +314,11 @@ class StoryContent extends StatelessWidget {
       ),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true; // 👈 mantiene vivo el widget
 }
+
 
 class NetworkImageWithCallback extends StatefulWidget {
   final String url;
@@ -322,6 +375,18 @@ class _NetworkImageWithCallbackState extends State<NetworkImageWithCallback> {
     }
     return Image.network(widget.url, fit: BoxFit.cover);
   }
+}
+
+class StoryPageInfo{
+  final String url;
+  final bool isVideo;
+  final Duration duration;
+
+  StoryPageInfo({
+    required this.url,
+    required this.isVideo,
+    required this.duration,
+  });
 }
 
 
